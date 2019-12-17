675da7a6215bb3b96e540d4c926972036cc20dd9
steampunkova <small.nihilist@yandex.ru>
2019-12-18 04:53:59 +0700

    7 пракическая

---
+++ 4 практическая.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace практическая_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 99; i++)
+            {
+                if (Math.Pow((i / 10) + (i % 10), 3) == Math.Pow(i, 2))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+}
