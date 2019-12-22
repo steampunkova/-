b6cafc6b35b0dca980c8afcff891b99b1f4ebc39
steampunkova <small.nihilist@yandex.ru>
2019-12-22 07:33:50 +0700

    4

---
+++ 4пр.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace елисей_4
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            {
+                var result = Enumerable.Range(100, 900)
+                  .Where(SumFactEq);
+                foreach (int i in result)
+                    Console.WriteLine("{0}\t", i);
+                Console.ReadKey();
+            }
+            static int fac(int i)
+            {
+                return i < 2 ? 1 : i * fac(i - 1);
+            }
+            static bool SumFactEq(int i)
+            {
+                int r = 0, t = i;
+                while (i > 0)
+                {
+                    r += fac(i % 10);
+                    i /= 10;
+                }
+                return r == t;
+            }
+        }
+    }
+}
+
