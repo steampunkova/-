a3c55fd3a2338b6121363023c0576ee43e6cb39f
steampunkova <small.nihilist@yandex.ru>
2019-12-26 01:08:15 +0700

    Create 3пр.cs

---
+++ 3пр.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ел_и_сей
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double e = Convert.ToDouble(Console.ReadLine());
+            double b = 1;
+            double s = 0;
+            int i = 0;
+            while (b >= e)
+            {
+                i++;
+                b = (1.0 / (i * i));
+                s += b;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
